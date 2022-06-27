882c566df925f410a05d97bf284198e4f2d073bd
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-27 14:52:21 +0000

    initial commit + file setup

---
+++ taskmanager/__init__.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):  
+    import env  # as this is in the ignore file, file needs to be found to be imported
+
+# environment variables
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")   
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+# instance import sqlalchemy class, set to instance of flask app
+db = SQLAlchemy(app)
+
+# needs to be imported after the above
+from taskmanager import routes  # noqa
