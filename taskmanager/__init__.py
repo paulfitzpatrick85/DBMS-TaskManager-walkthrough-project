4d5dd46af9911a8984ad6938cae59df7d7f7460c
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-30 12:22:52 +0000

    update heroku database in init file

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re  # regular expression package for python
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):  
@@ -11,7 +12,12 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")  # get uri from heroku
+    if uri.startswitch("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+
+    
 # instance import sqlalchemy class, set to instance of flask app
 db = SQLAlchemy(app)
 
